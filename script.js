22c6cd879b3f8d4e4c0ed22827fab27332e7d165
William Lim <manvinedi@gmail.com>
2017-12-19 04:46:17 +0800

    update

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
+var body = document.querySelector('body')
 var canvas = document.createElement('canvas')
 canvas.height = 512
 canvas.width = 512
-document.querySelector('body').appendChild(canvas)
+var statusText = document.createElement('div')
+body.appendChild(canvas)
+body.appendChild(statusText)
 var ctx = canvas.getContext('2d')
-ctx.fillStyle = 'green'
-ctx.fillRect(32, 32, 32, 32)
+function draw (pos) {
+  ctx.fillStyle = 'green'
+  ctx.fillRect(pos[0][0] * 32, pos[0][1] * 32, 32, 32)
+  ctx.fillStyle = 'red'
+  ctx.fillRect(pos[1][0] * 32, pos[1][1] * 32, 32, 32)
+}
+var pos = [[1, 1], [14, 14]]
+draw(pos)
 const grid = []
 for (let y = 0; y < 16; y++) {
   let row = []
@@ -13,11 +22,13 @@ for (let y = 0; y < 16; y++) {
   }
   grid.push(row)
 }
-console.log(grid)
-function step () {
 
+function step (x, y) {
+  ctx.clearRect(0, 0, canvas.height, canvas.width)
+  var newPos = [[1, 1], [x, y]]
+  draw(newPos)
+  statusText.textContent = 'player moved to position x: ' + x + ' y: ' + y
 }
 window.addEventListener('click', function (event) {
-  ctx.clearRect(0, 0, canvas.height, canvas.width)
-  ctx.fillRect(event.pageX - 16, event.pageY - 16, 32, 32)
+  step(Math.floor(event.pageX/512 * 16), Math.floor(event.pageY/512 * 16))
 }, false)
